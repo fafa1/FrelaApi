b7bcc2c5f753f23c2750dd84aa1152ff6bef3b50
Fagner <bfagner@hotmail.com>
2019-06-27 01:03:37 -0300

    atualizado

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/Users')
 
 const auth = async(req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer', '')
-    const data = jwt.verify(token, process.env.JWT_KEY)
+    const token = req.header('Authorization').replace('Bearer ', '')
+    const data = jwt.verify(token, "data")
     try {
         // verificando na minha lista de token se tal token existe
         const user = await User.findOne({ _id: data._id, 'tokens.token': token})
