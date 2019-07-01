563944026d1fbefec291fd08a742bfdad2686fa2
Fagner <bfagner@hotmail.com>
2019-07-01 01:07:42 -0300

    1 commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,37 +1,45 @@
 const express = require('express')
 const User = require('../models/Users')
 const auth = require('../middleware/auth')
+const cors = require('cors')
 
 const router = express.Router()
 
-router.get('/users/home', auth ,async (req, res) =>{
+const corsOptions = {
+    origin: 'http://localhost:8080/?#/cadastro',
+    optionsSuccessStatus: 200 
+}
+
+router.get('/home', auth ,async (req, res) => {
     // por meio do token
     res.send(req.user)
 } )
 
-router.post('/users',async (req, res) => {
+router.post('/users', cors(corsOptions), async (req, res) => {
     console.log(req.body)
     try {
         const user = new User(req.body)
         await user.save()
         const token = await user.generateAuthToken()
-        //console.log(token)
         res.status(201).send({ user, token })
+        // res.setHeader('Content-Type', 'application/json')
+        res.redirect('/home')
     } catch (error) {
-        res.status(400).send("error"+error)
+        res.status(400).send("error "+ error)
     }
 })
 
-router.post('/users/login', async (req, res) => {
-    console.log(req.body)
+router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body
         const user = await User.findByCredentials(email, password)
         if(!user)
-            return res.status(401).send({error: 'login falhou'})
+            return res.status(401).send({error: 'Usuário não encontrado'})
         
+        console.log(req.body)
         const token = await user.generateAuthToken()
-        res.send({ user, token })        
+        res.send({ user, token })
+        //res.redirect('/')
         } catch (error) {
         res.status(400).send(error)
     }
